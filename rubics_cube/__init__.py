7676fc0a74f3c983bfe663a4f6ebc80929c27968
osbm <osmanfbayram@gmail.com>
2023-02-17 15:46:13 +0300

    add version in the namespace

--- rubics_cube/__init__.py
+++ rubics_cube/__init__.py
@@ -1,1 +1,6 @@
 from .cube import Cube
+
+# make sure that the version is available in the package namespace
+module_path = __file__.split("__init__.py", maxsplit=1)[0]
+__version__ = open(module_path + "VERSION", encoding="utf-8").read().strip()
+del module_path
